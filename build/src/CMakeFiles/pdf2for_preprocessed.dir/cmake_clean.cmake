7bea58b74976bfe74681cc22f9750877ba53816d
Daniel <daniel.hammerschmid@documatrix.com>
2015-10-27 14:09:29 +0100

    Removed build folder
    Added .gitignore File

--- build/src/CMakeFiles/pdf2for_preprocessed.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pdf2for_preprocessed"
-  "pdf2for.vala"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/pdf2for_preprocessed.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/pdf2for.vala
+++
@@ -1,6 +0,0 @@
-public int main( string[] args )
-{
-  stdout.printf( "Converting PDF to FOR...\n" );
-  stdout.printf( "... done\n" );
-  return 0;
-}
