c1399de6fdf2bfe44a4c2de3cb162ee69b26f5af
documatrix <documatrix@localhost.localdomain>
2016-03-31 12:33:28 +0200

    pdflib cross compile error for windows fixed

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -5,6 +5,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER   i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(PKG_CONFIG_EXECUTABLE i686-w64-mingw32-pkg-config)
 
 
 # where is the target environment 
@@ -13,5 +14,5 @@ SET(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32 /usr/i686-w64-mingw32/sys-root/m
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
